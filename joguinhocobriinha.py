05dbed24fe9cf53dab530f4d7b152867357932a5
oaaoj <lui.oaoj@gmail.com>
2025-06-05 19:11:35 -0300

    attCobrinha funcionando

--- joguinhocobriinha.py
+++ joguinhocobriinha.py
@@ -12,21 +12,28 @@ green = (0,128,0)
 red = (255,0,0)
 black = (0,0,0)
 white = (255,255,255)
+yellow = (255,255,102)
+blue = (50,153,213)
 
 #tela
 
 d_height = 600
 d_width = 800
 displ_ay = pygame.display.set_mode((d_width,d_width))
-pygame.display.update()
 pygame.display.set_caption('Cobrinha')
 
 snake_sqr = 10 # largura da cobrinha
-snake_move = 20
+snake_move = 20 # velocidade da cobrinha
 
 clock = pygame.time.Clock()
 
-font_type = pygame.font.SysFont(None, 50)
+font_type = pygame.font.SysFont(None, 20)
+font_score_type = pygame.font.SysFont(None, 30)
+
+def cobrinha(snake_sqr, snake_tail):
+    for x in snake_tail:
+        pygame.draw.rect(displ_ay, green, [x[0], x[1], snake_sqr, snake_sqr])
+
 def message(msg, color):
     messeger = font_type.render(msg,True,color)
     displ_ay.blit(messeger, [d_width/3, d_width/3])
@@ -41,6 +48,9 @@ def game_loop():
     x1_loc = 0
     y1_loc = 0
 
+    snake_Tail = []
+    tail_of_snake = 1
+
     food_x = round(random.randrange(0, d_width - snake_sqr)/10.0) * 10.0
     food_y = round(random.randrange(0, d_width - snake_sqr)/10.0) * 10.0
 
@@ -52,7 +62,7 @@ def game_loop():
             message("Você perdeu! Aperte S para sair ou R para reiniciar", red)
             pygame.display.update()
 
-            for event.key in pygame.event.get():
+            for event in pygame.event.get():
                 if event.type==pygame.KEYDOWN:
                     if event.key==pygame.K_s:
                         game_over = True
@@ -78,22 +88,37 @@ def game_loop():
                     y1_loc = snake_sqr
                     x1_loc = 0
 
-        if x1 >= d_width or x1 < 0 or y1 >= d_height or y1 < 0:
-            game_over = True
+     
 
         x1 += x1_loc
         y1 += y1_loc
+
         displ_ay.fill(white)
         pygame.draw.rect(displ_ay, red,[food_x,food_y,snake_sqr,snake_sqr])
-        pygame.draw.rect(displ_ay, green,[x1,y1,snake_sqr,snake_sqr])
+        snake_head = []
+        snake_head.append(x1)
+        snake_head.append(y1)
+        snake_Tail.append(snake_head)
+
+        if len(snake_Tail) > tail_of_snake:
+            del snake_Tail[0]    
+        
+        for x in snake_Tail[:-1]:
+            if x == snake_head:
+                game_close = True
+
+        cobrinha(snake_sqr, snake_Tail)
+
         pygame.display.update()
 
         if x1 == food_x and y1 == food_y:
-            print("...")
+            food_x = round(random.randrange(0, d_width - snake_sqr)/10.0) * 10.0
+            food_y = round(random.randrange(0, d_width - snake_sqr)/10.0) * 10.0
 
+            tail_of_snake += 1
+            
         clock.tick(snake_move)
     
-    pygame.display.update()
     pygame.quit()
     quit()
 
